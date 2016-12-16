ee640369c37f6a6932fa75b9f3ca71547f4b73d4
Edward Jeffreys <edhjeffreys@outlook.com>
2016-12-16 15:24:14 +0000

    Sidebars collapse when screen size too small

--- js/homepage.js
+++ js/homepage.js
@@ -2,10 +2,10 @@
 $('[data-toggle="collapse-slide-left"]').on('click', function() {
     $navMenuCont = $($(this).data('target'));
     if($navMenuCont.attr("aria-expanded") === "true"){
-        $navMenuCont.animate({'left': '+=250px'}, 250);
+        $navMenuCont.animate({'left': '-=250px'}, 250);
         $navMenuCont.attr("aria-expanded", "false");
     }else{
-        $navMenuCont.animate({'left': '-=250px'}, 250);
+        $navMenuCont.animate({'left': '+=250px'}, 250);
         $navMenuCont.attr("aria-expanded", "true");
     }
 });
@@ -14,10 +14,33 @@ $('[data-toggle="collapse-slide-left"]').on('click', function() {
 $('[data-toggle="collapse-slide-right"]').on('click', function() {
     $navMenuCont = $($(this).data('target'));
     if($navMenuCont.attr("aria-expanded") === "true"){
-        $navMenuCont.animate({'right': '+=250px'}, 250);
+        $navMenuCont.animate({'right': '-=250px'}, 250);
         $navMenuCont.attr("aria-expanded", "false");
     }else{
-        $navMenuCont.animate({'right': '-=250px'}, 250);
+        $navMenuCont.animate({'right': '+=250px'}, 250);
         $navMenuCont.attr("aria-expanded", "true");
+    }
+});
+
+$(window).on('resize', function() {
+   if($(window).width() < 1000){
+       if($('[id="sidebar-left-wrapper"]').attr("aria-expanded") === "true"){
+            $('[id="sidebar-left-wrapper"]').animate({'left': '-=250px'}, 250);
+            $('[id="sidebar-left-wrapper"]').attr("aria-expanded", "false");
+       }
+       if($('[id="sidebar-right-wrapper"]').attr("aria-expanded") === "true"){
+            $('[id="sidebar-right-wrapper"]').css("visibility", "hidden");
+            $('[id="sidebar-right-wrapper"]').attr("aria-expanded", "false");
+       }
+    }
+    if($(window).width() >= 1000){
+        if($('[id="sidebar-left-wrapper"]').attr("aria-expanded") === "false"){
+            $('[id="sidebar-left-wrapper"]').animate({'left': '250px'});
+            $('[id="sidebar-left-wrapper"]').attr("aria-expanded", "true");
+       }
+       if($('[id="sidebar-right-wrapper"]').attr("aria-expanded") === "false"){
+            $('[id="sidebar-right-wrapper"]').css("visibility", "visible");
+            $('[id="sidebar-right-wrapper"]').attr("aria-expanded", "true");
+       }
     }
 });
